2f36cbbd8572e444c6093b2fc6881b11c18e3022
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-08-18 22:48:10 -0600

    refactor: Extracts server definition from main

--- main.go
+++ main.go
@@ -17,60 +17,41 @@ func main() {
 		log.Println("Error loading .env file")
 	}
 
-	// Auth setup
-	username := os.Getenv("USERNAME")
-	password := os.Getenv("PASSWORD")
-	jwtSecret := os.Getenv("JWT_SECRET")
-	tokenDurationSeconds := 60 * 60 // 1 hour
-
-	// Database setup
-	dbHost := envOrDefault("DATABASE_HOST", "localhost")
-	dbPort := envOrDefault("DATABASE_PORT", "5432")
-	dbUser := envOrDefault("DATABASE_USERNAME", "postgres")
-	dbPassword := envOrDefault("DATABASE_PASSWORD", "postgres")
-	dbName := envOrDefault("DATABASE_NAME", "postgres")
-	dbSsl := envOrDefault("DATABASE_SSL", "disable")
+	// Database
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		dbHost, dbUser, dbPassword, dbName, dbPort, dbSsl,
+		envOrDefault("DATABASE_HOST", "localhost"),
+		envOrDefault("DATABASE_PORT", "5432"),
+		envOrDefault("DATABASE_USERNAME", "postgres"),
+		envOrDefault("DATABASE_PASSWORD", "postgres"),
+		envOrDefault("DATABASE_NAME", "postgres"),
+		envOrDefault("DATABASE_SSL", "disable"),
 	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("%s", err)
+		log.Fatal(err)
 	}
 
-	db.AutoMigrate(&his{}, &rec{})
+	serverConfig := ServerConfig{
+		username:             os.Getenv("USERNAME"),
+		password:             os.Getenv("PASSWORD"),
+		jwtSecret:            os.Getenv("JWT_SECRET"),
+		tokenDurationSeconds: 60 * 60, // 1 hour
 
-	// Register paths
-	server := http.NewServeMux()
-	authController := authController{
-		jwtSecret:            jwtSecret,
-		tokenDurationSeconds: tokenDurationSeconds,
-		username:             username,
-		password:             password,
+		db:            db,
+		dbAutoMigrate: true,
 	}
-	server.HandleFunc("GET /auth/token", authController.getAuthToken)
-
-	tokenAuth := http.NewServeMux()
-
-	hisController := hisController{db: db}
-	tokenAuth.HandleFunc("GET /his/{pointId}", hisController.getHis)
-	tokenAuth.HandleFunc("POST /his/{pointId}", hisController.postHis)
-	tokenAuth.HandleFunc("DELETE /his/{pointId}", hisController.deleteHis)
-	server.Handle("/his/", tokenAuthMiddleware(jwtSecret, tokenAuth))
-
-	recController := recController{db: db}
-	tokenAuth.HandleFunc("GET /recs", recController.getRecs)
-	tokenAuth.HandleFunc("POST /recs", recController.postRecs)
-	tokenAuth.HandleFunc("GET /recs/tag/{tag}", recController.getRecsByTag)
-	tokenAuth.HandleFunc("GET /recs/{id}", recController.getRec)
-	tokenAuth.HandleFunc("PUT /recs/{id}", recController.putRec)
-	tokenAuth.HandleFunc("DELETE /recs/{id}", recController.deleteRec)
-	server.Handle("/recs/", tokenAuthMiddleware(jwtSecret, tokenAuth))
+	server, err := NewServer(serverConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	port := 8080
 	log.Printf("Serving at http://localhost:%d", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), server)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
 }
 
 func envOrDefault(name string, def string) string {
---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+
+	"gorm.io/gorm"
+)
+
+type ServerConfig struct {
+	// Auth
+	username             string
+	password             string
+	jwtSecret            string
+	tokenDurationSeconds int
+
+	// Database
+	db            *gorm.DB
+	dbAutoMigrate bool
+}
+
+func NewServer(serverConfig ServerConfig) (*http.ServeMux, error) {
+
+	if serverConfig.dbAutoMigrate {
+		err := serverConfig.db.AutoMigrate(&his{}, &rec{})
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Register paths
+	server := http.NewServeMux()
+	authController := authController{
+		jwtSecret:            serverConfig.jwtSecret,
+		tokenDurationSeconds: serverConfig.tokenDurationSeconds,
+		username:             serverConfig.username,
+		password:             serverConfig.password,
+	}
+	server.HandleFunc("GET /auth/token", authController.getAuthToken)
+
+	tokenAuth := http.NewServeMux()
+
+	hisController := hisController{db: serverConfig.db}
+	tokenAuth.HandleFunc("GET /his/{pointId}", hisController.getHis)
+	tokenAuth.HandleFunc("POST /his/{pointId}", hisController.postHis)
+	tokenAuth.HandleFunc("DELETE /his/{pointId}", hisController.deleteHis)
+	server.Handle("/his/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
+
+	recController := recController{db: serverConfig.db}
+	tokenAuth.HandleFunc("GET /recs", recController.getRecs)
+	tokenAuth.HandleFunc("POST /recs", recController.postRecs)
+	tokenAuth.HandleFunc("GET /recs/tag/{tag}", recController.getRecsByTag)
+	tokenAuth.HandleFunc("GET /recs/{id}", recController.getRec)
+	tokenAuth.HandleFunc("PUT /recs/{id}", recController.putRec)
+	tokenAuth.HandleFunc("DELETE /recs/{id}", recController.deleteRec)
+	server.Handle("/recs/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
+
+	return server, nil
+}
