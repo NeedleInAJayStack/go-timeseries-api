1ab1d3f0ac517b880fafa2898883c8ad2ffffa09
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-08-22 23:20:04 -0600

    fix: Fixes database connection

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	// Database
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		envOrDefault("DATABASE_HOST", "localhost"),
 		envOrDefault("DATABASE_PORT", "5432"),
 		envOrDefault("DATABASE_USERNAME", "postgres"),
