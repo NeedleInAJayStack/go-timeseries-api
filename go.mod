7b1c78332ab950a8a1ebca477a05a9095aa50f9f
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-11-09 23:38:01 -0700

    feat: Adds prometheus metrics

--- go.mod
+++ go.mod
@@ -3,17 +3,33 @@ module needleinajaystack/api
 go 1.22.6
 
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/prometheus/client_golang v1.20.5
+	github.com/stretchr/testify v1.9.0
 	gorm.io/datatypes v1.2.1
 	gorm.io/driver/postgres v1.5.9
+	gorm.io/driver/sqlite v1.5.6
 	gorm.io/gorm v1.25.11
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.57.0 // indirect
+	go.opentelemetry.io/otel v1.32.0 // indirect
+	go.opentelemetry.io/otel/metric v1.32.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.32.0 // indirect
+	go.opentelemetry.io/otel/sdk/metric v1.32.0 // indirect
+	go.opentelemetry.io/otel/trace v1.32.0 // indirect
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -22,13 +38,20 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.60.1 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	go.opentelemetry.io/otel/exporters/prometheus v0.54.0
+	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.32.0
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
-	gorm.io/driver/sqlite v1.5.6 // indirect
 )
--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"gorm.io/gorm"
 )
 
@@ -18,7 +19,7 @@ type ServerConfig struct {
 	dbAutoMigrate bool
 }
 
-func NewServer(serverConfig ServerConfig) (*http.ServeMux, error) {
+func NewServer(serverConfig ServerConfig) (http.Handler, error) {
 
 	if serverConfig.dbAutoMigrate {
 		err := serverConfig.db.AutoMigrate(&his{}, &rec{})
@@ -41,41 +42,47 @@ func NewServer(serverConfig ServerConfig) (*http.ServeMux, error) {
 	recController := recController{store: newGormRecStore(serverConfig.db)}
 	currentController := currentController{store: newInMemoryCurrentStore()}
 
+	// handleFunc is a replacement for mux.HandleFunc that adds route data to the metrics.
+	handleFunc := func(mux *http.ServeMux, pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
 	// All are deprecated. Instead use /api/*
-	server.HandleFunc("GET /auth/token", authController.getAuthToken)
-	tokenAuth.HandleFunc("GET /his/{pointId}", hisController.getHis)       // Deprecated
-	tokenAuth.HandleFunc("POST /his/{pointId}", hisController.postHis)     // Deprecated
-	tokenAuth.HandleFunc("DELETE /his/{pointId}", hisController.deleteHis) // Deprecated
-	tokenAuth.HandleFunc("GET /recs", recController.getRecs)
-	tokenAuth.HandleFunc("POST /recs", recController.postRecs)
-	tokenAuth.HandleFunc("GET /recs/tag/siteMeter", recController.getRecsByTag) // Deprecated. Use /recs?tag=""
-	tokenAuth.HandleFunc("GET /recs/{id}", recController.getRec)
-	tokenAuth.HandleFunc("PUT /recs/{id}", recController.putRec)
-	tokenAuth.HandleFunc("DELETE /recs/{id}", recController.deleteRec)
-	tokenAuth.HandleFunc("GET /recs/{pointId}/history", hisController.getHis)
-	tokenAuth.HandleFunc("POST /recs/{pointId}/history", hisController.postHis)
-	tokenAuth.HandleFunc("DELETE /recs/{pointId}/history", hisController.deleteHis)
-	tokenAuth.HandleFunc("GET /recs/{pointId}/current", currentController.getCurrent)
-	tokenAuth.HandleFunc("POST /recs/{pointId}/current", currentController.postCurrent)
+	handleFunc(server, "GET /auth/token", authController.getAuthToken)
+	handleFunc(tokenAuth, "GET /his/{pointId}", hisController.getHis)       // Deprecated
+	handleFunc(tokenAuth, "POST /his/{pointId}", hisController.postHis)     // Deprecated
+	handleFunc(tokenAuth, "DELETE /his/{pointId}", hisController.deleteHis) // Deprecated
+	handleFunc(tokenAuth, "GET /recs", recController.getRecs)
+	handleFunc(tokenAuth, "POST /recs", recController.postRecs)
+	handleFunc(tokenAuth, "GET /recs/tag/siteMeter", recController.getRecsByTag) // Deprecated. Use /recs?tag=""
+	handleFunc(tokenAuth, "GET /recs/{id}", recController.getRec)
+	handleFunc(tokenAuth, "PUT /recs/{id}", recController.putRec)
+	handleFunc(tokenAuth, "DELETE /recs/{id}", recController.deleteRec)
+	handleFunc(tokenAuth, "GET /recs/{pointId}/history", hisController.getHis)
+	handleFunc(tokenAuth, "POST /recs/{pointId}/history", hisController.postHis)
+	handleFunc(tokenAuth, "DELETE /recs/{pointId}/history", hisController.deleteHis)
+	handleFunc(tokenAuth, "GET /recs/{pointId}/current", currentController.getCurrent)
+	handleFunc(tokenAuth, "POST /recs/{pointId}/current", currentController.postCurrent)
 	server.Handle("/his/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 	server.Handle("/recs", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 	server.Handle("/recs/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 
-	server.HandleFunc("GET /api/auth/token", authController.getAuthToken)
-	tokenAuth.HandleFunc("GET /api/his/{pointId}", hisController.getHis)       // Deprecated
-	tokenAuth.HandleFunc("POST /api/his/{pointId}", hisController.postHis)     // Deprecated
-	tokenAuth.HandleFunc("DELETE /api/his/{pointId}", hisController.deleteHis) // Deprecated
-	tokenAuth.HandleFunc("GET /api/recs", recController.getRecs)
-	tokenAuth.HandleFunc("POST /api/recs", recController.postRecs)
-	tokenAuth.HandleFunc("GET /api/recs/tag/siteMeter", recController.getRecsByTag) // Deprecated. Use /recs?tag=""
-	tokenAuth.HandleFunc("GET /api/recs/{id}", recController.getRec)
-	tokenAuth.HandleFunc("PUT /api/recs/{id}", recController.putRec)
-	tokenAuth.HandleFunc("DELETE /api/recs/{id}", recController.deleteRec)
-	tokenAuth.HandleFunc("GET /api/recs/{pointId}/history", hisController.getHis)
-	tokenAuth.HandleFunc("POST /api/recs/{pointId}/history", hisController.postHis)
-	tokenAuth.HandleFunc("DELETE /api/recs/{pointId}/history", hisController.deleteHis)
-	tokenAuth.HandleFunc("GET /api/recs/{pointId}/current", currentController.getCurrent)
-	tokenAuth.HandleFunc("POST /api/recs/{pointId}/current", currentController.postCurrent)
+	handleFunc(server, "GET /api/auth/token", authController.getAuthToken)
+	handleFunc(tokenAuth, "GET /api/his/{pointId}", hisController.getHis)       // Deprecated
+	handleFunc(tokenAuth, "POST /api/his/{pointId}", hisController.postHis)     // Deprecated
+	handleFunc(tokenAuth, "DELETE /api/his/{pointId}", hisController.deleteHis) // Deprecated
+	handleFunc(tokenAuth, "GET /api/recs", recController.getRecs)
+	handleFunc(tokenAuth, "POST /api/recs", recController.postRecs)
+	handleFunc(tokenAuth, "GET /api/recs/tag/siteMeter", recController.getRecsByTag) // Deprecated. Use /recs?tag=""
+	handleFunc(tokenAuth, "GET /api/recs/{id}", recController.getRec)
+	handleFunc(tokenAuth, "PUT /api/recs/{id}", recController.putRec)
+	handleFunc(tokenAuth, "DELETE /api/recs/{id}", recController.deleteRec)
+	handleFunc(tokenAuth, "GET /api/recs/{pointId}/history", hisController.getHis)
+	handleFunc(tokenAuth, "POST /api/recs/{pointId}/history", hisController.postHis)
+	handleFunc(tokenAuth, "DELETE /api/recs/{pointId}/history", hisController.deleteHis)
+	handleFunc(tokenAuth, "GET /api/recs/{pointId}/current", currentController.getCurrent)
+	handleFunc(tokenAuth, "POST /api/recs/{pointId}/current", currentController.postCurrent)
 	server.Handle("/api/his/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 	server.Handle("/api/recs", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 	server.Handle("/api/recs/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
@@ -83,5 +90,8 @@ func NewServer(serverConfig ServerConfig) (*http.ServeMux, error) {
 	// Catch all others with public files. Not found fallback is app index for browser router.
 	server.Handle("/app/", fileServerWithFallback(http.Dir("./public"), "./public/app/index.html"))
 
-	return server, nil
+	// Observability
+	observed := otelhttp.NewHandler(server, "/")
+
+	return observed, nil
 }
