48ecc621c13d3249e0bc97ed75115419d132333b
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-11-09 22:23:02 -0700

    docs: Specifies fileserver usage

--- fileServer.go
+++ fileServer.go
@@ -1,10 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // fileServerWithFallback returns a file server that falls back to a specific file when no matches are found.
 // This is useful when a frontend app is using a browser-router.
 func fileServerWithFallback(dir http.Dir, fallbackFile string) http.Handler {
+	// Note that this fileserver matches the full route, so it should not be pointed to a subdirectory of the public folder.
 	fileserver := http.FileServer(dir)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, err := dir.Open(r.URL.Path)
