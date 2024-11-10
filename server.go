c78fe66195e0ae540acf6d7911dd2850e9754bc7
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-11-09 22:23:17 -0700

    feat: Changes to serve from `/api/`

--- server.go
+++ server.go
@@ -29,43 +29,59 @@ func NewServer(serverConfig ServerConfig) (*http.ServeMux, error) {
 
 	// Register paths
 	server := http.NewServeMux()
+	tokenAuth := http.NewServeMux()
+
 	authController := authController{
 		jwtSecret:            serverConfig.jwtSecret,
 		tokenDurationSeconds: serverConfig.tokenDurationSeconds,
 		username:             serverConfig.username,
 		password:             serverConfig.password,
 	}
-	server.HandleFunc("GET /auth/token", authController.getAuthToken)
-
-	tokenAuth := http.NewServeMux()
-
 	hisController := hisController{store: newGormHistoryStore(serverConfig.db)}
+	recController := recController{store: newGormRecStore(serverConfig.db)}
+	currentController := currentController{store: newInMemoryCurrentStore()}
+
+	// All are deprecated. Instead use /api/*
+	server.HandleFunc("GET /auth/token", authController.getAuthToken)
 	tokenAuth.HandleFunc("GET /his/{pointId}", hisController.getHis)       // Deprecated
 	tokenAuth.HandleFunc("POST /his/{pointId}", hisController.postHis)     // Deprecated
 	tokenAuth.HandleFunc("DELETE /his/{pointId}", hisController.deleteHis) // Deprecated
-	server.Handle("/his/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
-
-	recController := recController{store: newGormRecStore(serverConfig.db)}
 	tokenAuth.HandleFunc("GET /recs", recController.getRecs)
 	tokenAuth.HandleFunc("POST /recs", recController.postRecs)
-	server.Handle("/recs", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
-
 	tokenAuth.HandleFunc("GET /recs/tag/siteMeter", recController.getRecsByTag) // Deprecated. Use /recs?tag=""
 	tokenAuth.HandleFunc("GET /recs/{id}", recController.getRec)
 	tokenAuth.HandleFunc("PUT /recs/{id}", recController.putRec)
 	tokenAuth.HandleFunc("DELETE /recs/{id}", recController.deleteRec)
-
 	tokenAuth.HandleFunc("GET /recs/{pointId}/history", hisController.getHis)
 	tokenAuth.HandleFunc("POST /recs/{pointId}/history", hisController.postHis)
-	tokenAuth.HandleFunc("DELETE /recs/{pointId}/history", hisController.getHis)
-
-	currentController := currentController{store: newInMemoryCurrentStore()}
+	tokenAuth.HandleFunc("DELETE /recs/{pointId}/history", hisController.deleteHis)
 	tokenAuth.HandleFunc("GET /recs/{pointId}/current", currentController.getCurrent)
 	tokenAuth.HandleFunc("POST /recs/{pointId}/current", currentController.postCurrent)
+	server.Handle("/his/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
+	server.Handle("/recs", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 	server.Handle("/recs/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
 
+	server.HandleFunc("GET /api/auth/token", authController.getAuthToken)
+	tokenAuth.HandleFunc("GET /api/his/{pointId}", hisController.getHis)       // Deprecated
+	tokenAuth.HandleFunc("POST /api/his/{pointId}", hisController.postHis)     // Deprecated
+	tokenAuth.HandleFunc("DELETE /api/his/{pointId}", hisController.deleteHis) // Deprecated
+	tokenAuth.HandleFunc("GET /api/recs", recController.getRecs)
+	tokenAuth.HandleFunc("POST /api/recs", recController.postRecs)
+	tokenAuth.HandleFunc("GET /api/recs/tag/siteMeter", recController.getRecsByTag) // Deprecated. Use /recs?tag=""
+	tokenAuth.HandleFunc("GET /api/recs/{id}", recController.getRec)
+	tokenAuth.HandleFunc("PUT /api/recs/{id}", recController.putRec)
+	tokenAuth.HandleFunc("DELETE /api/recs/{id}", recController.deleteRec)
+	tokenAuth.HandleFunc("GET /api/recs/{pointId}/history", hisController.getHis)
+	tokenAuth.HandleFunc("POST /api/recs/{pointId}/history", hisController.postHis)
+	tokenAuth.HandleFunc("DELETE /api/recs/{pointId}/history", hisController.deleteHis)
+	tokenAuth.HandleFunc("GET /api/recs/{pointId}/current", currentController.getCurrent)
+	tokenAuth.HandleFunc("POST /api/recs/{pointId}/current", currentController.postCurrent)
+	server.Handle("/api/his/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
+	server.Handle("/api/recs", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
+	server.Handle("/api/recs/", tokenAuthMiddleware(serverConfig.jwtSecret, tokenAuth))
+
 	// Catch all others with public files. Not found fallback is app index for browser router.
-	server.Handle("/", fileServerWithFallback(http.Dir("./public"), "./public/index.html"))
+	server.Handle("/app/", fileServerWithFallback(http.Dir("./public"), "./public/app/index.html"))
 
 	return server, nil
 }
